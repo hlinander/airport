d0151e8bc33329d2a08344ce587aad00fba7c9a4
Rusty Conover <rusty@conover.me>
2026-05-21 08:34:07 -0400

    fix: bump copyright date

--- extension_config.cmake
+++ extension_config.cmake
@@ -6,13 +6,13 @@ duckdb_extension_load(airport
     LOAD_TESTS
 )
 
-#duckdb_extension_load(httpfs
-#    LOAD_TESTS
-#    DONT_LINK
-#    GIT_URL https://github.com/duckdb/duckdb-httpfs
-#    GIT_TAG 354d3f436a33f80f03a74419e76eb59459e19168
-#    INCLUDE_DIR extension/httpfs/include
-#)
+duckdb_extension_load(httpfs
+    LOAD_TESTS
+    DONT_LINK
+    GIT_URL https://github.com/duckdb/duckdb-httpfs
+    GIT_TAG 354d3f436a33f80f03a74419e76eb59459e19168
+    INCLUDE_DIR extension/httpfs/include
+)
 
 duckdb_extension_load(httpfs
     LOAD_TESTS
